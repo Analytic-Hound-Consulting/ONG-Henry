1f0495f04b8dbdf3bb59928eec782e50d1cbb081
Analytic Hound Consultants © <analytic.hound@gmail.com>
2023-03-21 16:02:02 -0300

    Update Home.py

--- Stream_finalisimo_kpi_last3/Home.py
+++ Stream_finalisimo_kpi_last3/Home.py
@@ -166,7 +166,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("style/style.css")
+local_css("./Stream_finalisimo_kpi_last3style/style.css")
 
 
 
